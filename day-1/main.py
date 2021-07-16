2ab90c9e9a22c99101c898d053ce97027aed16c4
anncannbann <anncannbann@gmail.com>
2021-07-16 16:55:44 +0530

    Fix The Code

--- day-1/main.py
+++ day-1/main.py
@@ -13,3 +13,13 @@ print("print('what to print')")
 
 #string manipulation and Code intelligence
 print('Hello ' +'User')
+
+#Fix the code below 👇
+'''
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+'''
+
+#input
