a70eaa30e18bcc4007cf24e8efc218f0e2004334
anncannbann <anncannbann@gmail.com>
2021-07-16 16:47:10 +0530

    String manipulation

---
+++ day-1/main.py
@@ -0,0 +1,15 @@
+#print('hello world!')
+
+#print function
+'''
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+'''
+
+#print("Day 1 - Python Print Function \n The function is declared like this: \n print('what to print')")
+
+
+
+#string manipulation and Code intelligence
+print('Hello ' +'User')
