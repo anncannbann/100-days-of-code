1c9c052107c56c77cf350f94883f17ea9670bc6a
anncannbann <anncannbann@gmail.com>
2021-07-17 13:02:45 +0530

    Data Types

---
+++ day-2/main.py
@@ -0,0 +1,21 @@
+#Data types
+
+#subscripting.....
+from typing_extensions import TypeVarTuple
+
+
+print("Hello"[4])
+
+#int
+print(123 +345)
+
+# we can write 123,234,234 as 
+print(123_234_234)
+
+
+#float
+3.14159
+
+#Boolean
+True
+False
