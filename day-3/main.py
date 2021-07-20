f3ed2262570014c2bb8f810510670c37cad3f78c
anncannbann <anncannbann@gmail.com>
2021-07-20 16:27:00 +0530

    BMI 2.0

--- day-3/main.py
+++ day-3/main.py
@@ -28,3 +28,27 @@ else:
 
 '''
 
+#BMI 2.0
+'''
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+h1 = float(height)
+w1 = float(weight)
+
+bmi = round(w1 / (h1 **2))
+
+if(bmi <18.5):
+    print(f'your BMI is ${bmi} , you are underweight')
+elif(bmi >18.5 and bmi< 25):
+    print(f'Your BMI is {bmi} andf you have a normal weight')
+elif(bmi > 25 and bmi <30):
+    print(f'Your BMI is {bmi}, you are slightly overrweight')
+elif(bmi > 30 and bmi <35):
+      print(f'Your BMI is {bmi}, you are Obese')
+else:
+      print(f'Your BMI is {bmi}, you are Clinically Obese.')
+    '''
+
+#leap year
