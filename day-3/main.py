89812d50f397c8b809f4786d5fa1e17b1f28de20
anncannbann <anncannbann@gmail.com>
2021-07-20 16:14:15 +0530

    Rollarcoaster 2.0

--- day-3/main.py
+++ day-3/main.py
@@ -1,16 +1,30 @@
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height in cm? "))
 
-
-# if height> 120:
-#     print('You Can Ride')
-# else:
-#     print('Sorry you tiny, cant ride')
-
-    
 # odd-even
-n = int(input('Enter a number'))
+'''
+n = int(input('Enter a number to check for even or odd?!'))
 if(n %2==0):
     print('number is Even.')
 else:
     print('Number is Odd.')
+'''
+
+#Rolercoaster ticket counter
+'''
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+
+if height> 120:
+    print('You are eligble for riding')
+    age = int(input("Enter your Age to Continue"))
+    if age >= 18:
+        print(' Your total is $12')
+    elif age >=12 and age< 18:
+        print('Your total is $7')
+    else:
+        print('Your total is $5')
+else:
+    print('Sorry you tiny, cant ride')
+
+'''
+
