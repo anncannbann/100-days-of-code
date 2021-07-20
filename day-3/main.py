dc498425364ec52b8cbe8235a60d941c5d23154f
anncannbann <anncannbann@gmail.com>
2021-07-20 17:23:15 +0530

    Pizza ordering System

--- day-3/main.py
+++ day-3/main.py
@@ -75,3 +75,34 @@ if height> 120:
 else:
     print('Sorry you tiny, cant ride')
 '''
+
+#pizza ordering
+print('Welcome to Python Pizza 🍕 ')
+print('********MENU********** \n Small Pizza: $15 \n Medium Pizza: $20 \n Large Pizza: $25 \n Pepperoni for Small Pizza: +$2 \n Pepperoni for Medium or Large Pizza: +$3 \n Extra cheese for any size pizza: + $1 \n')
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+bill =0
+#size
+if(size =='S'):
+    bill = 15
+elif(size =='M'):
+    bill = 20
+else:
+    bill = 25
+
+if(add_pepperoni =='Y'):
+    if size =='S':
+        bill+=2
+    else:
+        bill +=3
+
+if(extra_cheese =='Y'):
+    bill+=1
+
+print(f'Your Bill is ${bill}')
+
+
+
+
