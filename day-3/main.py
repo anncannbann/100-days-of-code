633d2d0390c38471a9924cec836c8109e3535e97
anncannbann <anncannbann@gmail.com>
2021-07-20 15:58:24 +0530

    First if-else

---
+++ day-3/main.py
@@ -0,0 +1,10 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+
+# if height> 120:
+#     print('You Can Ride')
+# else:
+#     print('Sorry you tiny, cant ride')
+
+    
