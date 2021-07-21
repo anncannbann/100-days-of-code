9f115d718176f97673f5355a531ca723d0b6c9de
anncannbann <anncannbann@gmail.com>
2021-07-21 14:16:55 +0530

    heads or tails using random

--- day-4/main.py
+++ day-4/main.py
@@ -1,7 +1,7 @@
 import random
 import my_module as p
 
-
+'''
 #randomisation
 x = random.randint(1,10)
 print(x)
@@ -9,4 +9,17 @@ print(x)
 print(p.pi)
 
 
+xy = random.random()*5
 
+print(xy)
+
+'''
+#heads or tails
+
+toss = random.randint(0,1)
+
+if(toss ==1):
+    print('Heads')
+else:
+    print('Tails')
+    
