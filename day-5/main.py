5b46d97bd4d83761884d74623862ab7918aa4958
anncannbann <anncannbann@gmail.com>
2021-07-23 15:31:46 +0530

    Avg without using sum and len function

--- day-5/main.py
+++ day-5/main.py
@@ -1,5 +1,19 @@
 #for loop
-
+'''
 fruits= ['apple','orange','banana']
 for fruit in fruits:
     print(fruit)
+'''
+
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+total =0
+no=0
+
+for i in range(0,len(student_heights)):
+    total += student_heights[i]
+    no +=1
+avg = round(total/no)
+print(f'Avg height is {avg}')
