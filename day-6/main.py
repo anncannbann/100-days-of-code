b5d920c8482f6f9454284f5e813e180c0258f9e5
anncannbann <anncannbann@gmail.com>
2021-07-24 22:55:47 +0530

    karel/ reeborg

--- day-6/main.py
+++ day-6/main.py
@@ -7,3 +7,4 @@ myfun()
 '''
 
 #Karel the Robot.
+#go to : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
