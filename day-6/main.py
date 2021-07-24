f9300de1a08c138e666772710dc4073f7a68ff64
anncannbann <anncannbann@gmail.com>
2021-07-24 23:44:29 +0530

    Day 6: Finished

--- day-6/main.py
+++ day-6/main.py
@@ -8,3 +8,5 @@ myfun()
 
 #Karel the Robot.
 #go to : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+#while loop
