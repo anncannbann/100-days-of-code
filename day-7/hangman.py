e024bc136f9e970e56b5577eddb1ce8ab89579c5
anncannbann <anncannbann@gmail.com>
2021-07-31 12:28:12 +0530

    Hangman :Finished

--- day-7/hangman.py
+++ day-7/hangman.py
@@ -1,27 +1,175 @@
-# import random
-# # #Step 1 
+#Step 4
 
-# word_list = ["aardvark", "baboon", "camel"]
+import random
 
-# # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
-# # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-# # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+lives = 6
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess != letter:
+        lives-=1
+
+    if lives == 0:
+        end_of_game = True
+        print(stages[0])
+        print('You lose!')
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    if(lives > 0):
+        print(stages[lives])
 
 
-# # #1
-# choosen_word = random.choice(word_list)
 
-# #2
-# guess = input('Enter a letter to check:\n').lower()
 
-# #3
-# for i in range(0, len(choosen_word)):
-#     if(choosen_word[i] == guess):
-#         print('Right')
-#     else:
-#         print('Wrong')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+import random
+# #Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+
+# #1
+choosen_word = random.choice(word_list)
+
+#2
+guess = input('Enter a letter to check:\n').lower()
+
+#3
+for i in range(0, len(choosen_word)):
+    if(choosen_word[i] == guess):
+        print('Right')
+    else:
+        print('Wrong')
+'''
 
 #Step 2
 
@@ -46,6 +194,10 @@ for position in range(0,len(chosen_word)):
 
 print(display)
 '''
+
+
+
+'''
 #step 3
 import random
 word_list = ["aardvark", "baboon", "camel"]
@@ -69,3 +221,4 @@ while not gameend:
     if "_" not in display:
         gameend = True
         print("YOU WON!!!!🏆")
+'''
