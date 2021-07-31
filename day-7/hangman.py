d3a3e0c258819c89f0e6199c09a08bfaf216778c
anncannbann <anncannbann@gmail.com>
2021-07-31 11:44:31 +0530

    Hangman :Displaying the guessed letter

--- day-7/hangman.py
+++ day-7/hangman.py
@@ -1,16 +1,16 @@
 # import random
-# #Step 1 
+# # #Step 1 
 
 # word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 
-# #1
+# # #1
 # choosen_word = random.choice(word_list)
 
 # #2
@@ -24,6 +24,7 @@
 #         print('Wrong')
 
 #Step 2
+
 
 import random
 word_list = ["aardvark", "baboon", "camel"]
@@ -39,27 +40,8 @@ for i in range(0,len(chosen_word)):
     display.append('_')
 print(display)
 
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
-
-while(display != chosen_word):
-
-    for i in range(0,len(chosen_word)):
-        if(guess ==chosen_word[i]):
-            display[i]= guess
-        else:
-            print('Wrong')
-
-if(display !=chosen_word):
-    print('You lost')
-else:
-    print('You win!')
-
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+for position in range(0,len(chosen_word)):
+    if (guess == chosen_word[position]):
+        display[position] = guess
 
 print(display)
-
-
-
